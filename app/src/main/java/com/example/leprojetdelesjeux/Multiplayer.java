6534b0663a9a38e7908bf47d7e8c2fff036b50fb
Whyvra <julien.ornat@hotmail.com>
2021-11-30 14:46:04 +0100

    first achri

---
+++ app/src/main/java/com/example/leprojetdelesjeux/Multiplayer.java
@@ -0,0 +1,14 @@
+package com.example.leprojetdelesjeux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Multiplayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplayer);
+    }
+}
